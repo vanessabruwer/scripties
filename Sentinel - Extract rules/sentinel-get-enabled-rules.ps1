073623928f3559d2c26dab205495c27683a3e372
Vanessa Bruwer <vanessa.bruwer@microsoft.com>
2022-01-07 12:14:27 +0000

    Added sample script for retrieving Sentinel Analytics rules

---
+++ Sentinel - Extract rules/sentinel-get-enabled-rules.ps1
@@ -0,0 +1,25 @@
+$ReportDate = Get-Date -format "yyyy-M-dd"
+$ReportLocation = "C:\temp"
+$ReportName = "Sentinel-Analytics-Rules-$ReportDate.csv"
+$SavetoFile = "$ReportLocation\$ReportName"
+
+
+$ResourceGroup = ""
+$WorkspaceName = ""
+
+
+# Check if folder exists, if not, create it
+ if (!(Test-Path $ReportLocation)){
+ 
+ New-Item $ReportLocation -type Directory | Out-Null
+
+ }
+
+# Delete any previous versions of the report for in case
+If (Test-Path $SavetoFile){
+	 Remove-Item $SavetoFile
+}
+
+
+get-azsentinelalertrule -ResourceGroupName "$ResourceGroup" -WorkspaceName "$WorkspaceName" | Select-Object DisplayName, Enabled, ProductFilter, Kind | Export-Csv $SavetoFile
+
---
+++ Sentinel - Extract rules/sentinel-get-rule-templates.ps1
@@ -0,0 +1,23 @@
+$ReportDate = Get-Date -format "yyyy-M-dd"
+$ReportLocation = "C:\temp"
+$ReportName = "Sentinel-Analytics-RuleTemplates-$ReportDate.csv"
+$SavetoFile = "$ReportLocation\$ReportName"
+
+$ResourceGroup = ""
+$WorkspaceName = ""
+
+# Check if folder exists, if not, create it
+ if (!(Test-Path $ReportLocation)){
+ 
+ New-Item $ReportLocation -type Directory | Out-Null
+
+ }
+
+# Delete any previous versions of the report for in case
+
+If (Test-Path $SavetoFile){
+	 Remove-Item $SavetoFile
+}
+
+
+get-azsentinelalertruletemplate -ResourceGroupName "$ResourceGroup" -WorkspaceName "$WorkspaceName" | Select-Object DisplayName, Status, Kind, RequiredDataConnectors, Severity | Export-Csv $SavetoFile
